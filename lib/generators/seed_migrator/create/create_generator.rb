2c339c71e37e9acbc6764212920333e54c03db5e
baburdick <github.com@bruceburdick.com>
2014-09-28 21:28:23 -0500

    v0.4.1: Fix create_generator for Rails4
    Task 1256

--- lib/generators/seed_migrator/create/create_generator.rb
+++ lib/generators/seed_migrator/create/create_generator.rb
@@ -5,13 +5,16 @@ require 'generators/seed_migrator/helper'
 class SeedMigrator::CreateGenerator < Rails::Generators::NamedBase
   include Generators::SeedMigrator::Helper
   include Rails::Generators::Migration
-  extend ActiveRecord::Generators::Migration
-  
+
   source_root File.expand_path('../templates', __FILE__)
 
   # Creates the data update file and the migration file.
   def create_helper_file
     migration_template "data_update.rb", "db/data_updates/#{file_name}_data_update.rb"
     migration_template "data_update_migration.rb", "db/migrate/#{file_name}.rb"
+  end
+
+  def self.next_migration_number dirname
+    ActiveRecord::Generators::Base.next_migration_number dirname
   end
 end
